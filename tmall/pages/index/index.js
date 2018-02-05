7e2616d7575a9d01f179c16bc52bef28191a73da
yiyinsong <390396674@qq.com>
2018-02-05 18:31:04 +0800

    2018-2-5

--- tmall/pages/index/index.js
+++ tmall/pages/index/index.js
@@ -1,55 +1,52 @@
 //index.js
 //获取应用实例
+import Util from '../../utils/util';
 
 const app = getApp()
+const util = new Util();
 
 Page({
   data: {
-    motto: 'Hello World5',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    imgUrls: [
+      '../../assets/images/uploads/b1.jpg',
+      '../../assets/images/uploads/b2.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 300,
+    systemInfo: util.systemInfo(),
+    headerHeight: 82,
+    scrollTimer: null,
+    countDownTime: 10,
+    countDown: {
+      hour: 0,
+      minute: 0,
+      second: 0
     }
   },
-  getUserInfo (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  onLoad () {
+    this.countDown();
+  },
+  fnBodyScroll (e) {
+    this.data.scrollTimer && clearTimeout(this.data.scrollTimer);
+    this.data.scrollTimer = setTimeout(() => {
+      if(e.detail.scrollTop > 50) {
+        this.setData({headerHeight: 46});
+      } else {
+        this.setData({headerHeight: 82});
+      }
+    }, 500);
+  },
+  countDown() { 
+    if(this.data.countDownTime >= 0) {
+      this.setData({
+        countDown: util.countDownFormatTime(this.data.countDownTime),
+        countDownTime: this.data.countDownTime - 1
+      });
+      setTimeout(() => {
+        this.countDown();
+      }, 1000);
+    }
   }
 })
--- tmall/utils/util.js
+++ tmall/utils/util.js
@@ -1,19 +1,21 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
+export default class Util {
+  constructor() {
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
+  }
 
-module.exports = {
-  formatTime: formatTime
+  systemInfo() {
+    return wx.getSystemInfoSync();
+  }
+
+  countDownFormatTime(stamp) {
+    const _hour = Math.floor(stamp / 3600);
+    const _minute = Math.floor((stamp - _hour * 3600) / 60);
+    const _second = (stamp - _hour * 3600) - _minute * 60;
+    return {
+      hour: _hour < 10 ? '0' + _hour : _hour.toString(),
+      minute: _minute < 10 ? '0'+ _minute : _minute.toString(),
+      second: _second < 10 ? '0' + _second : _second.toString()
+    }
+  }
 }
